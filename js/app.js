ae652cadbc68700e9b27e88fc83ea82977d58d82
Hridoy <rakibhasanhridoy776@gmail.com>
2022-03-01 20:17:33 +0600

    modifiction

--- js/app.js
+++ js/app.js
@@ -22,24 +22,27 @@ const loadPhones = searchText => {
 function displayPhones(phones) {
     const divContainer = document.getElementById('div-container');
     divContainer.textContent = '';
-    phones.forEach(phone => {
-        // console.log(phone)
-        const container = document.createElement('div');
-        container.innerHTML = `
-        <div class="col rounded-5 border-secondary">
-                  <div class="card h-75">
-                    <img src="${phone.image}" class="card-img-top w-100" alt="...">
-                    <div class="card-body">
-                      <h4 class="card-title">${phone.phone_name}</h4>
-                      <h5 class="card-title">${phone.brand}</h5>
-                      <button onclick="loadDetails('${phone.slug}')" class="btn btn-primary">Details</button>
-                    </div>
-                  </div>
-                </div>
-        `;
-        divContainer.appendChild(container);
-    });
-
+    if(phones.length!=0){
+        phones?.slice(0,20).forEach(phone => {
+            const container = document.createElement('div');
+            container.innerHTML = `
+            <div class="col rounded-5 border-secondary">
+               <div class="card h-75">
+                 <img src="${phone.image}" class="card-img-top w-100" alt="...">
+                 <div class="card-body">
+                   <h4 class="card-title">${phone.phone_name}</h4>
+                   <h5 class="card-title">${phone.brand}</h5>
+                   <button onclick="loadDetails('${phone.slug}')" class="btn btn-primary">Details</button>
+                 </div>
+               </div>
+             </div>
+            `;
+            divContainer.appendChild(container);
+        });
+    }
+    else{
+        alert('hello')
+    }
 }
 
 // load details
@@ -51,22 +54,22 @@ const loadDetails =slug=>{
     .then(data=>displayDetails(data.data))
     
 }
+// dispay details
 const displayDetails =details =>{
     console.log(details)
     const detailsContainer =document.getElementById('details');
     detailsContainer.textContent ='';
     detailsContainer.innerHTML =`
     <div class=" w-50 rounded">
-                    <img src="${details.image}" class=" img-fluid" alt="...">
-                    <div class="card-body">
-                      <h4 class="card-title">${details.name}</h4>
-                      <h5 class="card-title">${details.releaseDate}</h5>
-                      
-                      <p class="card-text"><strong>Chipset:</strong>${details.mainFeatures.chipSet},<strong>Display:</strong>${details.mainFeatures.displaySize},<strong>Memory:</strong>${details.mainFeatures.memory}</p>
-                      <p class="card-text"><strong>Sensors:</strong>${details.mainFeatures.sensors[0]},${details.mainFeatures.sensors[1]},${details.mainFeatures.sensors[2]},${details.mainFeatures.sensors[3]},${details.mainFeatures.sensors[4]},${details.mainFeatures.sensors[5]}</p>
-                      <p class="card-text"><strong>Others:</strong>${details.others.Bluetooth},${details.others.GPS}.${details.others.NFC},${details.others.Radio},${details.others.USB},${details.others.WLAN}</p>
-                    </div>
-                  </div>
+         <img src="${details.image}" class=" img-fluid" alt="...">
+         <div class="card-body">
+           <h4 class="card-title">${details.name}</h4>
+           <h5 class="card-title">${details.releaseDate ? details.releaseDate:'release date not found' }</h5>
+           <p class="card-text"><strong>Chipset:</strong>${details.mainFeatures.chipSet},<strong>Display:</strong>${details.mainFeatures.displaySize},<strong>Memory:</strong>${details.mainFeatures.memory}</p>
+           <p class="card-text"><strong>Sensors:</strong>${details.mainFeatures.sensors[0]},${details.mainFeatures.sensors[1]},${details.mainFeatures.sensors[2]},${details.mainFeatures.sensors[3]},${details.mainFeatures.sensors[4]},${details.mainFeatures.sensors[5]}</p>
+           <p class="card-text"><strong>Others:</strong>${details?.others?.Bluetooth ? details?.others?.Bluetooth:'not found'},${details?.others?.GPS ? details?.others?.GPS:''}.${details?.others?.NFC ? details?.others?.NFC:''},${details?.others?.Radio ? details?.others?.Radio:''},${details?.others?.USB ? details?.others?.USB:''},${details?.others?.WLAN ? details?.others?.WLAN:''}</p>
+         </div>
+    </div>
     `;
 }
 
